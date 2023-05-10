f9a1669cd42ad0470cd48d813cb878071b4e98fb
markus-senger <senger1995@gmail.com>
2023-05-10 14:03:06 +0200

    added sudoku test after solving

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -16,6 +16,11 @@ void TestOneSudoku(fs::path const& path) {
 	std::cout << (solver.Solve(path, true) ? "\nSolved = True" : "\nSolved = False") << "\n\n";
 
 	solver.Print();
+
+	if (!solver.CheckSolvedSudoku())
+		std::cout << "ERROR: Sudoku not correct!\n\n";
+	else
+		std::cout << "Sudoku correct!\n\n";
 }
 
 void TestAllSudokus(fs::path const& path) {
@@ -27,6 +32,11 @@ void TestAllSudokus(fs::path const& path) {
 			std::cout << "--- " << entry << " -----------------------------\n";
 			std::cout << (solver.Solve(entry, false) ? "\nSolved = True" : "\nSolved = False") << "\n\n";
 			solver.SaveInFile("./Results/Result" + entry.path().filename().string() + ".txt");
+		
+			if (!solver.CheckSolvedSudoku())
+				std::cout << "ERROR: Sudoku not correct!\n\n";
+			else
+				std::cout << "Sudoku correct!\n\n";
 		}
 	}
 }
